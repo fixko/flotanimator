39fbff3d8aa598655ba40ef8e8a2c75caa075350
Chtiwi Malek <admin@monoprog.com>
2013-10-15 20:14:32 +0200

    Update and rename flotanimator.js to jquery.flot.animator.js

--- jquery.flot.animator.js
+++ jquery.flot.animator.js
@@ -1,10 +1,11 @@
 /* jQuery Flot Animator version 1.0.
 
+Flot Animator is a free jQuery Plugin that will add fluid animations to Flot charts.
+
 Copyright (c) 2012-2013 Chtiwi Malek
 http://www.codicode.com/art/jquery_flot_animator.aspx
 
 Licensed under Creative Commons Attribution 3.0 Unported License.
-
 */
 
 $.extend({
